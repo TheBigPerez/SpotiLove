21107d36f6f07712afa2201308f5d764038f4dfa
DonyAgadony <donybof1@gmail.com>
2026-02-05 09:37:26 +0200

    fixed last commit

---
+++ Migrations/20260205073514_IdToUUID.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JsonDemo.Migrations
+{
+    /// <inheritdoc />
+    public partial class IdToUUID : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            if (migrationBuilder.ActiveProvider == "Npgsql.EntityFrameworkCore.PostgreSQL")
+            {
+                migrationBuilder.Sql(@"
+ALTER TABLE ""Users""
+    ALTER COLUMN ""Id"" TYPE uuid USING ""Id""::uuid;
+
+ALTER TABLE ""MusicProfiles""
+    ALTER COLUMN ""Id"" TYPE uuid USING ""Id""::uuid,
+    ALTER COLUMN ""UserId"" TYPE uuid USING ""UserId""::uuid;
+
+ALTER TABLE ""UserImages""
+    ALTER COLUMN ""Id"" TYPE uuid USING ""Id""::uuid,
+    ALTER COLUMN ""UserId"" TYPE uuid USING ""UserId""::uuid;
+
+ALTER TABLE ""Likes""
+    ALTER COLUMN ""FromUserId"" TYPE uuid USING ""FromUserId""::uuid,
+    ALTER COLUMN ""ToUserId"" TYPE uuid USING ""ToUserId""::uuid;
+
+ALTER TABLE ""UserSuggestionQueues""
+    ALTER COLUMN ""UserId"" TYPE uuid USING ""UserId""::uuid,
+    ALTER COLUMN ""SuggestedUserId"" TYPE uuid USING ""SuggestedUserId""::uuid;
+
+ALTER TABLE ""Messages""
+    ALTER COLUMN ""Id"" TYPE uuid USING ""Id""::uuid,
+    ALTER COLUMN ""FromUserId"" TYPE uuid USING ""FromUserId""::uuid,
+    ALTER COLUMN ""ToUserId"" TYPE uuid USING ""ToUserId""::uuid;
+");
+            }
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
