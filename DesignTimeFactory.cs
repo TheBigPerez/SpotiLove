f5e260d5923d74429f2ba41e8940b8ab66bf91c8
DonyAgadony <donybof1@gmail.com>
2026-05-19 10:52:24 +0300

    changed db creation sequence

--- DesignTimeFactory.cs
+++ DesignTimeFactory.cs
@@ -10,36 +10,30 @@ public class DesignTimeFactory : IDesignTimeDbContextFactory<AppDbContext>
     {
         var builder = new DbContextOptionsBuilder<AppDbContext>();
 
-        var connectionString =
-            Environment.GetEnvironmentVariable("DatabaseURL")
-            ?? "Data Source=spotilove.db";
+        // Try direct connection string first
+        var connStr = Environment.GetEnvironmentVariable("ConnectionStrings__PostgresConnection");
 
-        if (connectionString.StartsWith("postgres://") || connectionString.StartsWith("postgresql://"))
+        if (string.IsNullOrEmpty(connStr))
         {
-            var databaseUri = new Uri(connectionString);
-            var userInfo = databaseUri.UserInfo.Split(':', 2);
+            // Fall back to URL parsing
+            var url = Environment.GetEnvironmentVariable("DATABASE_URL")
+                      ?? throw new Exception("No database connection configured");
 
-            // Fix database name parsing
-            var dbName = databaseUri.AbsolutePath.TrimStart('/');
-            if (dbName.Contains('?'))
-            {
-                dbName = dbName.Substring(0, dbName.IndexOf('?'));
-            }
+            var uri = new Uri(url);
+            var userInfo = uri.UserInfo.Split(':', 2);
 
-            var connStrBuilder = new Npgsql.NpgsqlConnectionStringBuilder
-            {
-                Host = databaseUri.Host,
-                Port = databaseUri.Port > 0 ? databaseUri.Port : 5432,
-                Database = dbName,
-                Username = userInfo[0],
-                Password = userInfo[1],
-                SslMode = Npgsql.SslMode.Require,
-                TrustServerCertificate = true
-            };
-
-            builder.UseNpgsql(connStrBuilder.ConnectionString)
-                   .UseSnakeCaseNamingConvention();
+            connStr =
+                $"Host={uri.Host};" +
+                $"Port={uri.Port};" +
+                $"Database={uri.AbsolutePath.TrimStart('/')};" +
+                $"Username={userInfo[0]};" +
+                $"Password={userInfo[1]};" +
+                $"SSL Mode=Require;" +
+                $"Trust Server Certificate=true";
         }
+
+        builder.UseNpgsql(connStr)
+               .UseSnakeCaseNamingConvention();
 
         return new AppDbContext(builder.Options);
     }
